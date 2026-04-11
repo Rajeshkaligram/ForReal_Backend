178a0063296b1385251e2fcd7bd5135ddaba4f0b
sonaiStyloPay <sonai@stylopay.com>
2026-04-12 01:52:05 +0530

    env and db change

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                defined('PDO::MYSQL_ATTR_SSL_CA') ? PDO::MYSQL_ATTR_SSL_CA : 1001 => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
